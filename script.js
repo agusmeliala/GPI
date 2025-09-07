368bd6c13c9375244c94df4d16ef9e540f87ebd3
agusmeliala <aguschandra366@gmail.com>
2025-09-07 20:32:09 +0700

    update poster

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function updateDateTime() {
   const now = new Date();
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',
+                    hour: '2-digit', minute: '2-digit', second: '2-digit' };
   document.getElementById('datetime').textContent = now.toLocaleDateString('id-ID', options);
 }
 setInterval(updateDateTime, 1000);
